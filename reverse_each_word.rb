770980327fd10fbfe103b37bf7475cc811e8d846
Danny Tseng <dtfocus1025@gmail.com>
2019-04-08 03:35:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   s_array = sentence.split(" ")
+  temp = []
   s_array.each do |word|
-    word.split("").reverse.join
-    
+    temp.push(word.split("").reverse.join)
   end
-  
+  temp
 end
